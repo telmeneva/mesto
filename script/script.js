09b86a0271e1a5205e3ba96f9a6a52020cdf3ad4
Telmeneva Alina <linka140198@gmail.com>
2022-04-21 17:34:32 +0300

    Фикс после первого ревью

--- script/script.js
+++ script/script.js
@@ -1,14 +1,18 @@
 const popup = document.querySelector('.popup');
-const edit = document.querySelector('.info__edit');
+const edit = document.querySelector('.profile__edit');
 const close = popup.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__proff');
+let nameArea = document.querySelector('.profile__name');
+let jobArea = document.querySelector('.profile__proff');
 
-function popupOpenInfo() {
-    popup.classList.toggle('popup_is-opened');
+function OpenPopupInfo() {
+    popup.classList.add('popup_is-opened');
 }
-
-edit.addEventListener('click', popupOpenInfo);
-
-close.addEventListener('click', popupOpenInfo);
+function ClosePopupInfo() {
+    popup.classList.remove('popup_is-opened');
+}
 
 // const likes = document.getElementsByClassName('element__like');
 
@@ -21,28 +25,18 @@ close.addEventListener('click', popupOpenInfo);
 //     const like = likes[i];
 //     like.addEventListener('click', likeActive);
 // }
-
-let formElement = document.querySelector('.popup__form')
-let nameInput = document.querySelector('.popup__name')
-let jobInput = document.querySelector('.popup__proff')
-
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-
-    let nameArea = document.querySelector('.info__name');
-    let jobArea = document.querySelector('.info__proff');
-
-    nameArea.textContent = nameInput.value;
-    jobArea.textContent = jobInput.value;
-}
-
-formElement.addEventListener('submit', formSubmitHandler);
-
-
-const save = document.querySelector('.popup__save');
-
 function closePopup() {
     popup.classList.remove('popup_is-opened');
 }
 
-save.addEventListener('click', closePopup);
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+
+    nameArea.textContent = nameInput.value;
+    jobArea.textContent = jobInput.value;
+    closePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+edit.addEventListener('click', OpenPopupInfo);
+close.addEventListener('click', ClosePopupInfo);
